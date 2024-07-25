8d396bed9b3cfd1f499c519da15a42603a02f1c1
Liza Polovaya <liza.polovaya@gmail.com>
2024-07-25 16:29:36 +0300

    classname fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home</h1>
 
-      <div class="multiselect">
+      <div className="multiselect">
     <label><input type="checkbox" name="" value="1" /> Hydration - 1.5L of water a day </label> <br />
     <label><input type="checkbox" name="" value="2" /> Going to bed before 12 AM</label> <br />
     <label><input type="checkbox" name="" value="3" /> Meditation - 10 mins </label> <br />
